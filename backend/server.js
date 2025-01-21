037d770e89220225106eac4bda989b21cb55818c
ianjure <ianjuremacalisang2@gmail.com>
2025-01-21 14:48:20 +0800

    creating product API route

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.post("/products", async (req, res) => {
+    const product = req.body; // req.body is the data that is sent with the POST request by the user
+
+    if(!product.name || !product.price || !product.image) { // check if any of the fields are missing
+        return res.status(400).json({ success: false, message: "Please provide all fields." });
+    }
+
+    const newProduct = new Product(product); // create a new product with the data sent by the user
+
+    try {
+        await newProduct.save(); // save the product to the database
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        console.error("Error in creating product: ", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
 
 app.listen(5000, () => {
     connectDB();
