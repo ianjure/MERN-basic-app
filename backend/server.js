ec4beff8926dae2844d8a6ace9398bbb8d37bbf7
ianjure <ianjuremacalisang2@gmail.com>
2025-01-21 14:26:12 +0800

    added mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+app.get('/products', (req, res) => {});
+
+console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
   console.log('Server is running on http://localhost:5000');
 });
 
+// TO RUN: npm run dev
