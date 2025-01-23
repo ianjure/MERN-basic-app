461ef1980dd973462cc0ec2b83b989d04eb6aaa5
ianjure <ianjuremacalisang2@gmail.com>
2025-01-23 12:54:07 +0800

    designed the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,43 @@
+import { PlusSquareIcon } from '@chakra-ui/icons';
+import { Container, Flex, HStack, Button, Text } from '@chakra-ui/react';
 import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container
+        maxW={"1140px"}
+        px={4}
+        >
+        <Flex
+            h={16}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            flexDir={{ base: "column", md: "row" }}
+            >
+            <Text
+                fontSize={{ base: "22", sm: "28" }}
+                fontWeight={"bold"}
+                textTransform={"uppercase"}
+                textAlign={"center"}
+                bgGradient={"linear(to-r, cyan.400, blue.500)"}
+                bgClip={"text"}
+                >
+                <Link to={"/"}>
+                    Store 🛒
+                </Link>
+            </Text>
+            <HStack
+                spacing={4}
+                alignItems={"center"}
+                >
+                <Link to={"/create"}>
+                    <Button>
+                        <PlusSquareIcon fontSize={20}/>
+                    </Button>
+                </Link>
+            </HStack>
+        </Flex>
+    </Container>
   )
 };
 
