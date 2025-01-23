07795e93fe8381f5e094842c37c28c24f3587195
ianjure <ianjuremacalisang2@gmail.com>
2025-01-23 09:24:02 +0800

    created delete route

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const app = express(); // create an instance of express
 
 app.use(express.json()); // allows us to accept JSON data in the body of the request (req.body)
 
+// Route: POST /api/products - Create a Product
 app.post("/api/products", async (req, res) => {
     const product = req.body; // req.body is the data that is sent with the POST request by the user
 
@@ -23,11 +24,32 @@ app.post("/api/products", async (req, res) => {
         res.status(201).json({ success: true, data: newProduct });
     } catch (error) {
         console.error("Error in creating product: ", error.message);
-        res.status(500).json({ success: false, message: "Server Error" });
+        res.status(500).json({ success: false, message: "Internal server error." });
     }
 });
 
-// CREATE NEW ENDPOINT HERE
+// Route: DELETE /api/products - Delete a Product
+app.delete("/api/products/:id", async (req, res) => { // :id is a URL parameter (dynamic and can be any value)
+    const {id} = req.params; // get the id from the URL - {what you passed in the URL}
+
+    try {
+        await Product.findByIdAndDelete(id); // find the product by id through the Product model then delete it
+        res.status(200).json({ success: true, message: "Product deleted successfully." });
+    } catch (error) {
+        res.status(404).json({ success: false, message: "Product not found." });
+    }
+});
+
+// Route: GET /api/products - Get all Products
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find(); // find all products in the database
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+        console.error("Error in getting products: ", error.message);
+        res.status(500).json({ success: false, message: "Internal server error." });
+    }
+});
 
 app.listen(5000, () => { // start the server on port 5000
     connectDB(); // connect to the database
