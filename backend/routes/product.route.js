de78bb37340d922ec97981e014ab16e970c3c875
ianjure <ianjuremacalisang2@gmail.com>
2025-01-23 10:22:51 +0800

    added more comments

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -4,12 +4,10 @@ import { getProducts, createProduct, updateProduct, deleteProduct } from "../con
 
 const router = express.Router(); // create a new router
 
-router.get("/", getProducts); // use the getProducts controller to handle the request
+// PRODUCTS: define the routes and the controllers to handle the requests
+router.get("/", getProducts); // use the getProducts controller
+router.post("/", createProduct); // use the createProduct controller
+router.put("/:id", updateProduct); // use the updateProduct controller
+router.delete("/:id", deleteProduct); // use the deleteProduct controller
 
-router.post("/", createProduct); // use the createProduct controller to handle the request
-
-router.put("/:id", updateProduct); // use the updateProduct controller to handle the request
-
-router.delete("/:id", deleteProduct); // use the deleteProduct controller to handle the request
-
-export default router; // export the router so that it can be used in other files, using default we can import it with any name in other files
+export default router; // export using default so we can import it with any name in other files
