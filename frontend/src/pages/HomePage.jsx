2ef61073210f6d6aa015214dbf3a3330e87816ad
ianjure <ianjuremacalisang2@gmail.com>
2025-01-23 17:01:44 +0800

    created the initial home page design

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,39 @@
+import { Container, VStack, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <Container maxW="container.xl" py={12}>
+            <VStack spacing={8}>
+                <Text
+                    fontSize={"30"}
+                    fontWeight={"bold"}
+                    bgGradient={"linear(to-r, cyan.400, blue.500)"}
+                    bgClip={"text"}
+                    textAlign={"center"}
+                    >
+                    Products 📦
+                </Text>
+
+                <Text
+                    fontSize={"xl"}
+                    fontWeight={"bold"}
+                    color={"gray.500"}
+                    textAlign={"center"}
+                    >
+                    No products found 😢{" "}
+                    <Link to={"/create"}>
+                        <Text
+                            as={"span"}
+                            color={"blue.500"}
+                            _hover={{ textDecoration: "underline" }}
+                            >
+                            Create one now!
+                        </Text>
+                    </Link>
+                </Text>
+            </VStack>
+        </Container>
     )
 };
 
