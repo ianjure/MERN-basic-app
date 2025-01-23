a7cc58f31e142b52535acd496401613892b120ce
ianjure <ianjuremacalisang2@gmail.com>
2025-01-23 13:26:33 +0800

    changed the background format

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
+import { useColorModeValue } from '@chakra-ui/react';
 
 import Navbar from "./components/Navbar.jsx";
 import HomePage from "./pages/HomePage.jsx";
@@ -7,7 +8,7 @@ import CreatePage from "./pages/CreatePage.jsx";
 
 function App() {
   return (
-    <Box minH={"100vh"}>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
