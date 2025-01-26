8c3f1d47ee24d6a0c79f4334d0da2bb1fa30f46c
ianjure <ianjuremacalisang2@gmail.com>
2025-01-26 19:30:33 +0800

    initialize pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Box, useColorModeValue } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 
+import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
 import Navbar from "./components/Navbar.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import CreatePage from "./pages/CreatePage.jsx";
@@ -10,6 +12,8 @@ function App() {
     <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       <Navbar />
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
       </Routes>
---
+++ frontend/src/pages/SignupPage.jsx
@@ -0,0 +1,89 @@
+import { Container, VStack, Heading, Box, Input, Button, useToast, useColorModeValue } from "@chakra-ui/react";
+import { useState } from "react";
+import { useProductStore } from "../store/product.js";
+
+const CreatePage = () => {
+    const [newProduct, setNewProduct] = useState({ // newProduct is an object with three properties: name, price, and image
+        name: "",
+        price: "",
+        image: "",
+    });
+
+    const toast = useToast(); // use the toast function from Chakra UI
+    const { createProduct } = useProductStore(); // get the created createProduct hook
+
+    const handleAddProduct = async () => { // function to handle adding a new product
+        const {success, message} = await createProduct(newProduct); // call the createProduct hook
+
+        if(!success) { // if the product was not created successfully, show an error toast
+            toast({
+                title: "Error",
+                description: message,
+                status: "error",
+                duration: 2500,
+                isClosable: true
+            })
+        } else { // if the product was created successfully, show a success toast
+            toast({
+                title: "Success",
+                description: message,
+                status: "success",
+                duration: 2500,
+                isClosable: true
+            })
+        }
+        setNewProduct({ name: "", price: "", image: "" }); // reset the input fields
+    };
+
+    return (
+        <Container maxW={"container.sm"}>
+            <VStack spacing={2}>
+                <Heading
+                    as={"h1"}
+                    size={"2xl"}
+                    textAlign={"center"}
+                    mt={12}
+                    mb={4}
+                    >
+                    Create New Product
+                </Heading>
+
+                <Box
+                    w={"full"}
+                    bg={useColorModeValue("white", "gray.800")}
+                    p={6}
+                    rounded={"lg"}
+                    shadow={"lg"}
+                    >
+                    <VStack spacing={4}>
+                        <Input
+                            name="name"
+                            placeholder="Product Name"
+                            value={newProduct.name}
+                            onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+                            />
+                        <Input
+                            name="price"
+                            placeholder="Price"
+                            type="number"
+                            value={newProduct.price}
+                            onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+                            />
+                        <Input
+                            name="image"
+                            placeholder="Image URL"
+                            value={newProduct.image}
+                            onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
+                            />
+
+                        <Button colorScheme="blue" onClick={handleAddProduct} w="full">
+                            Add Product
+                        </Button>
+                    </VStack>
+                </Box>
+            </VStack>
+        </Container>
+    )
+};
+
+export default CreatePage;
